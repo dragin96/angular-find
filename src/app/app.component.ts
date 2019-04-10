9e6293867fad3aadace8642bc7338fdfc9053f5c
dragin96 <alexxx55509@mail.ru>
2019-04-10 19:35:58 +0300

    add find

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
   name = 'Angular';
